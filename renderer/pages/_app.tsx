fd10d02d02c7dd91101510d72081b4f3e076347c
Person1234565 <josephkan14@gmail.com>
2024-11-26 17:44:22 -0800

    add tailwindcss

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from "next/app";
-import Layout from "../components/layout";
+import Layout from "./layout";
+import "./globals.css";
+
 const App: React.FC<AppProps> = ({
   Component,
   pageProps: { session, ...pageProps },
--- renderer/pages/layout.tsx
+++ renderer/pages/layout.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import styles from "./layout.module.css";
 
 function Layout({ children }: { children: React.ReactNode }) {
