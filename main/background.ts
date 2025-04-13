a8e2ac3043127885e755d44cbd6e9b18bdb05310
Edward <bepbananas@gmail.com>
2025-04-12 19:09:51 -0700

    saving data now works, can upload json files to restore data

--- main/background.ts
+++ main/background.ts
@@ -1,8 +1,9 @@
 import path from 'path'
-import { app, ipcMain } from 'electron'
+import { app, ipcMain, dialog } from 'electron'
 import serve from 'electron-serve'
 import { createWindow } from './helpers'
 import * as fs from "fs";
+import { readFile } from "fs/promises";
 
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -49,4 +50,54 @@ ipcMain.on('save-file', async (event, { filename, content }) => {
     event.reply("save-file-error", "Failed to save file.");
   }
 })
+
+ipcMain.handle('save-dialog', async (event, data) => {
+	const { filePath, canceled } = await dialog.showSaveDialog({
+		title: 'Save File',
+		defaultPath: 'data.json',
+		filters: [{ name: 'JSON Files', extensions: ['json'] }]
+	});
+	return canceled ? null : filePath;
+});
+
+// FOR VERIFY PAGE \/ \/ \/
   
+ipcMain.handle('read-file-for-verification', async (event, filePath: string) => {
+	try {
+		const buffer = await readFile(filePath); // or no encoding for Buffer
+		const transportedBuffer = buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
+		return {
+			name: path.basename(filePath), // get filename only
+			extension: path.extname(filePath),
+			filePath: filePath,
+			data: transportedBuffer,
+		};
+	} catch (err) {
+		return {};
+	}
+});
+
+ipcMain.handle('pick-files-for-verification', async (event) => { // need to do this to get FULL file paths
+	const result = await dialog.showOpenDialog({
+	  	properties: ['openFile', 'multiSelections'],
+		filters: [{ name: 'All Files', extensions: ['json', 'mp3', 'wav'] }],
+	});
+
+	if (result.canceled) {
+		return [];
+	}
+
+	const filesWithData = await Promise.all(
+		result.filePaths.map(async (filePath) => {
+			const buffer = await readFile(filePath); // returns Buffer
+			const transportedBuffer = buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
+			return {
+				name: path.basename(filePath), // get filename only
+				extension: path.extname(filePath),
+				filePath: filePath,
+				data: transportedBuffer,
+			};
+		})
+	);
+	return filesWithData; 
+});
